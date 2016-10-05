ca9a12585aa8a6e651c77900109637be7110b76b
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-04 22:56:58 -0400

    find which graduates had vpn accounts

---
+++ vpn_expire_list_v1.py
@@ -0,0 +1,25 @@
+# goal: given a list of graduates and a list of vpn account holders,
+# find which vpn accounts should be expired
+
+import csv
+
+with open('examples/vpn_list.csv', 'rb') as f:
+    reader = csv.reader(f)
+    vpn_accounts = list(reader)
+
+with open('examples/graduates.csv', 'rb') as f:
+    reader = csv.reader(f)
+    graduates = list(reader)
+
+vpn_accounts_to_expire = []
+graduate_user_ids = graduates[0]
+vpn_account_ids = vpn_accounts[0]
+
+for graduate in graduate_user_ids:
+	vpn_account_name = "vpn-{}".format(graduate)
+	if  vpn_account_name in vpn_account_ids:
+		vpn_accounts_to_expire.append(vpn_account_name)
+
+with open("examples/vpn_accounts_to_expire_v1.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerow(vpn_accounts_to_expire)
