8834b9d778c874845022d95857c861c269393ead
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-04 23:01:34 -0400

    format comments

--- make_user_lists.py
+++ make_user_lists.py
@@ -34,16 +34,16 @@ college_leave = all_accounts[GRADUATE_COUNT : college_leave_index]
 deans_leave = all_accounts[college_leave_index : deans_leave_index]
 noise = all_accounts[deans_leave_index:]	
 
-#make a subset of those students as vpn account holders
+# make a subset of those students as vpn account holders
 graduates_with_vpn = random.sample(graduates, int(GRADUATE_COUNT * .3))
 college_leave_with_vpn = random.sample(college_leave, int(COLLEGE_LEAVE_COUNT * .1))
 deans_leave_with_vpn = random.sample(deans_leave, int(DEANS_LEAVE_COUNT * .5))
 
-#make the vpn list
+# make the vpn list
 vpn_list = list(graduates_with_vpn + college_leave_with_vpn + deans_leave_with_vpn + noise)
 vpn_list = [ "vpn-" + username for username in vpn_list]
 
-#write out all 4 lists
+# write out all 4 lists
 csv_writer("examples/graduates.csv", graduates)
 csv_writer("examples/college_leave.csv", college_leave)
 csv_writer("examples/deans_leave.csv", deans_leave)
