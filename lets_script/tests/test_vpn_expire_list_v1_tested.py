79196954dbe63c8649609a637494cb8e95d3f3a3
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-05 21:47:38 -0400

    add test suite

--- lets_script/tests/test_vpn_expire_list_v1_tested.py
+++ lets_script/tests/test_vpn_expire_list_v1_tested.py
@@ -1,3 +1,9 @@
 import pytest
-import lets_script.vpn_expire_list_v1_tested
+import lets_script.vpn_expire_list_v1_tested as expire_list
+
+
+def test_finds_matching_ids():
+	graduates = ['homer', 'marge', 'lisa', 'bart']
+	vpn_ids = ['vpn-homer', 'vpn-maggie', 'vpn-millhouse', 'vpn-lisa']
+	assert(expire_list.get_expire_list(graduates, vpn_ids) == ['vpn-homer', 'vpn-lisa'])
 
