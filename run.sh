8f9d35dab426535e5a9e80634cdb5e614adf0edc
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-05 21:45:50 -0400

    make a basic runner

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Naive Implementation
+python lets_script/vpn_expire_list_v1.py
+
+echo Refined Implementation
+python lets_script/vpn_expire_list_v1_refined.py
+
+echo Tested Implementation
+python lets_script/vpn_expire_list_v1_tested.py
