bcadc10d9611cfd5b4ce5e8e177f4d3e3b0a9317
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-05 21:47:13 -0400

    hook up tests

--- lets_script/vpn_expire_list_v1_tested.py
+++ lets_script/vpn_expire_list_v1_tested.py
@@ -3,12 +3,15 @@
 
 import csv
 
+GRADUATES = "examples/graduates.csv"
+VPN_ACCOUNTS = "examples/vpn_list.csv"
+OUTPUT_FILE = "examples/vpn_accounts_to_expire_v1_tested.csv"
+
 def get_ids_from_file(input_file):
 	with open(input_file, 'rb') as f:
 	    reader = csv.reader(f)
 	    users = list(reader)
 	return users[0]
-
 
 def get_expire_list(departing_users, vpn_account_ids):
 	vpn_accounts_to_expire = []
@@ -18,10 +21,11 @@ def get_expire_list(departing_users, vpn_account_ids):
 			vpn_accounts_to_expire.append(vpn_account_name)
 	return vpn_accounts_to_expire
 
-graduate_user_ids = get_ids_from_file("lets_script/examples/graduates.csv")
-vpn_account_ids = get_ids_from_file("lets_script/examples/vpn_list.csv")
-vpn_accounts_to_expire = get_expire_list(graduate_user_ids, vpn_account_ids)
+if __name__ == "__main__":
+	graduate_user_ids = get_ids_from_file(GRADUATES)
+	vpn_account_ids = get_ids_from_file(VPN_ACCOUNTS)
+	vpn_accounts_to_expire = get_expire_list(graduate_user_ids, vpn_account_ids)
 
-with open("lets_script/examples/vpn_accounts_to_expire_v1_refined.csv", "wb") as f:
-    writer = csv.writer(f)
-    writer.writerow(vpn_accounts_to_expire)
+	with open(OUTPUT_FILE, "wb") as f:
+	    writer = csv.writer(f)
+	    writer.writerow(vpn_accounts_to_expire)
