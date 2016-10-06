b23ddc649edbf638e4ce7d44f8d90a7e8071d26d
Sarah Gray <sarah.eliz.gray@gmail.com>
2016-10-05 22:29:36 -0400

    naive implementation of v2

---
+++ lets_script/vpn_expire_list_v2.py
@@ -0,0 +1,41 @@
+# goal: given a list of graduates, a list of students on college leave, 
+# and a list of vpn account holders,
+# find which vpn accounts should be expired
+
+import csv
+import sys
+import argparse
+
+
+def get_ids_from_file(input_file):
+	with open(input_file, 'rb') as f:
+	    reader = csv.reader(f)
+	    users = list(reader)
+	return users[0]
+
+def get_expire_list(departing_users, vpn_account_ids):
+	vpn_accounts_to_expire = []
+	for user in departing_users:
+		vpn_account_name = "vpn-{}".format(user)
+		if  vpn_account_name in vpn_account_ids:
+			vpn_accounts_to_expire.append(vpn_account_name)
+	return vpn_accounts_to_expire
+
+def write_file(output_file, vpn_accounts_to_expire):
+	with open(output_file, "wb") as f:
+	    writer = csv.writer(f)
+	    writer.writerow(vpn_accounts_to_expire)
+
+
+if __name__ == "__main__":
+	graduates = sys.argv[1]
+	college_leave = sys.argv[2]
+	vpn_accounts = sys.argv[3]
+	output_file = sys.argv[4]
+	
+	graduate_user_ids = get_ids_from_file(graduates)
+	college_leave_ids = get_ids_from_file(college_leave)
+	vpn_account_ids = get_ids_from_file(vpn_accounts)
+
+	vpn_accounts_to_expire = get_expire_list(graduate_user_ids + college_leave_ids, vpn_account_ids)
+	write_file(output_file, vpn_accounts_to_expire)
--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 
-echo Naive Implementation
+echo Naive Implementation 1
 rm lets_script/examples/vpn_accounts_to_expire_v1.csv
 python lets_script/vpn_expire_list_v1.py
 
-echo Refined Implementation
+echo Refined Implementation 1
 rm lets_script/examples/vpn_accounts_to_expire_v1_refined.csv
 python lets_script/vpn_expire_list_v1_refined.py
 
-echo Tested Implementation
+echo Tested Implementation 1
 rm lets_script/examples/vpn_accounts_to_expire_v1_tested.csv
 python lets_script/vpn_expire_list_v1_tested.py
+
+echo Naive Implementation 2
+rm lets_script/examples/vpn_accounts_to_expire_v2.csv
+python lets_script/vpn_expire_list_v2.py lets_script/examples/graduates.csv lets_script/examples/college_leave.csv lets_script/examples/vpn_list.csv lets_script/examples/vpn_accounts_to_expire_v2.csv
